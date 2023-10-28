e4800445488524e274a23a47218e3d9ded6aceea
Mariem_AMZIL <147107786+MAR-AM@users.noreply.github.com>
2023-10-28 01:00:27 +0100

    (ﾉ◕ヮ◕)ﾉ*:･ﾟ

---
+++ averge.py
@@ -0,0 +1,5 @@
+a=int(input('Enter the first number : '))
+b=int(input('Enter the second number : '))
+c=int(input('Enter the third number : '))
+print('The averge of these three integers entered is : ',(a+b+c)/3)
+
